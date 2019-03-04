af4ced1e10562802fc5fb85aa6552728b04e4d34
Kumar Abhinav <abhinav0441@gmail.com>
2019-03-04 09:24:41 +0530

    x

---
+++ type.js
@@ -0,0 +1,15 @@
+var num = 3.9;
+console.log(typeof num);
+num = 'Hello';
+console.log(typeof num);
+num = true;
+console.log(typeof num);
+console.log(typeof x);
+console.log(typeof {x: 2});
+
+var f = function(x, y) {
+    return x + y;
+}
+
+console.log(typeof f);
+
